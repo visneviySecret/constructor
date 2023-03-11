47468dabb2314558cce2dbe3e1e5f859221be42b
visneviySecret <visneviy.secret@yandex.ru>
2023-03-11 22:00:21 +0300

    update icons

--- public/assets/braces.tsx
+++ public/assets/braces.tsx
@@ -7,7 +7,7 @@ export const BraceActive = () => {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <title>brace logo</title>
+      <title>brace icon</title>
       <path
         d="M4.5 8.33334L1.16666 5.00001L4.5 1.66668M9.5 1.66668L12.8333 5.00001L9.5 8.33334"
         stroke="#5D5FEF"
@@ -28,7 +28,7 @@ export const Brace = () => {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <title>brace logo</title>
+      <title>brace icon</title>
       <path
         d="M4.5 8.33334L1.16666 5.00001L4.5 1.66668M9.5 1.66668L12.8333 5.00001L9.5 8.33334"
         stroke="#4D5562"
